02c16c8eae3386a99825743fea8e6a488815a8d7
CerealKiller <C3r3alK1ll3r@protonmail.com>
2017-11-24 23:48:04 -0600

    Delete twit.config.js

--- twit.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    //Your Twitter credentials go in this file
-    consumer_key:         '?',
-    consumer_secret:      '?',
-    access_token:         '?',
-    access_token_secret:  '?'
-  };
-  
