9227f3bbb90a39957b7bdf7c40fcb181452cc67e
Cra5hOv3r1d3 <Cra5hOv3r1d3@protonmail.com>
2017-11-24 23:47:15 -0600

    Ready for Lamda Deploy

--- handler-post-to(old).js
+++ handler-post-to(old).js
@@ -8,7 +8,19 @@ const LAST_ID_K = "Last ID";
 
 this.initBot = (status) => {
     console.log("status: ", status);
-    let tweet = "<Save the net! https://www.battleforthenet.com/>" + " @" + status.user.screen_name + " " + status.text;
+    console.log("----------------------------------------");
+    console.log("user_mentions: ", status.entities.user_mentions);
+    let mentions = status.entities.user_mentions;
+    let retweetList = "";
+    mentions.forEach((n) => {
+        console.log(n);
+        retweetList = retweetList + " @" + n.screen_name;
+    });
+    console.log("----------------------------------------");
+    console.log(retweetList);
+    retweetList = retweetList + " @" + status.user.screen_name;
+//   let tweet = "Go Here to learn more about saving theweb! @" + status.user.screen_name + " " + status.text;
+    let tweet = "Go Here to learn more about saving theweb! " + retweetList;
   console.log("tweet: ", tweet);
   console.log("----------------------------------------");
   T.post("statuses/update", {
@@ -29,7 +41,7 @@ this.initBot = (status) => {
 module.exports.saveTheWebBot = (event, context, callback) => {
     console.log("Hello?");
   T.get("search/tweets", {
-    q: "#NetNeutrality",
+    q: "#NNBTest",
     count: 1
   }, (errN, dataN, resN) => {
     if (errN) {
