3c3522b55ae6acf8e0dc79dac3394665f9df9881
C3r3alK1ll3r <C3r3alK1ll3r@protonmail.com>
2017-11-25 20:14:39 -0600

    Now retweeting 2 tweet about #NetNeutrality every 15 minutes

--- handler.js
+++ handler.js
@@ -6,18 +6,19 @@ module.exports.saveTheWebBot = (event, context, callback) => {
 
     let hashtag = "#NetNeutrality";
 
-    API.search(hashtag, 1).then((tweets) => {
+    API.search(hashtag, 2).then((tweets) => {
       //console.log(tweets);
 
       console.log("tweets[0].retweeted_status: ", tweets[0].retweeted_status);
 
-
+      //I found I have to us id_str instead of just id. For some reseaon the two
+      //  vars can very. id_str seems to the the original
       if(tweets[0].retweeted_status) {
-        console.log("tweets[0].retweeted_status.id: ", tweets[0].retweeted_status.id);
-        API.retweet(tweets[0].retweeted_status.id);
+        console.log("tweets[0].retweeted_status.id_str: ", tweets[0].retweeted_status.id_str);
+        API.retweet(tweets[0].retweeted_status.id_str);
       } else {
-        console.log("tweets[0].id: ", tweets[0].id);
-        API.retweet(tweets[0].id);
+        console.log("tweets[0].id_str: ", tweets[0].id_str);
+        API.retweet(tweets[0].id_str);
       }
 
 
