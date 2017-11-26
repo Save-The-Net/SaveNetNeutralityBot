6b86817cc25d29b7bf1c509f7b77981045bca9b7
C3r3alK1ll3r <C3r3alK1ll3r@protonmail.com>
2017-11-26 11:37:36 -0600

    osmose bot

--- handler.js
+++ handler.js
@@ -24,9 +24,8 @@ function retweetHashTags() {
 function tweetAboutHowToFight() {
 
   const isps = ["Comcast", "AT&T", "Verzion", "Charter Sprectrum", "Cricket", "CenturyLink", "Sprint", "T-Mobile"];
-
   const tweets = [
-    "Do you really want to start paying " + isps[Math.floor(Math.random() * isp.length)] + " an ADDITION fee just to check your twitter feed? No? Then write and call your representatives now https://www.battleforthenet.com/ and them want to keep #NetNeutrality Then retweet to all your followers!",
+    "Do you really want to start paying " + isps[Math.floor(Math.random() * isps.length)] + " an ADDITION fee just to check your twitter feed? No? Then write and call your representatives now https://www.battleforthenet.com/ and them want to keep #NetNeutrality Then retweet to all your followers!",
     "Tell the White House That You Want To Keep #NetNeutrality! Sign this White House petition and retweet to your followers! https://tinyurl.com/yc5vp5m3 #SaveNetNeutrality",
     "Get Ready, December 7th is the Don't Kill the Net Protest at Verzion stores. You can find one in your area here: http://verizonprotests.com/. Get everyone you know, this isn't a partisan issue. This is keeping the Net an open space for innovation! #NetNeutrality",
     "Only five people at the FCC get to vote on Net Neutrality Repeal. Two plan to vote keep net neutrality. To defeat the net neutrality repeal one of these men MUST change their vote: Ajit Pai: 202-518-7399 Michael O'Rielly: 301-657-9092 Brendan Carr: 202-719-7305 #NetNeutrality",
@@ -34,9 +33,9 @@ function tweetAboutHowToFight() {
     "Have you called Congress today? https://www.battleforthenet.com/ Take Action Now Before It's To Late! Call and Retweet to #SaveTheNet #SaveNetNeutrality #NetNeutrality #SaveTheNetBot",
     "Add your own #NetNeutrality Campaign Bot to Your Twitter Account. https://github.com/Save-Net-Neutrality/SaveNetNeutralityBot #SaveTheNet by keeping the word moving while you're on the move, or taking a break. #SaveTheNetBot will help everyone spread info about how to #SaveNetNeutrality and call on your followers to #DefendTheNet"
   ];
-  API.tweet(tweets[Math.floor(Math.random() * tweets.length]);
-  API.tweet(tweets[Math.floor(Math.random() * tweets.length]);
-  API.tweet(tweets[Math.floor(Math.random() * tweets.length]);
+  API.tweet(tweets[Math.floor(Math.random() * tweets.length)]);
+  API.tweet(tweets[Math.floor(Math.random() * tweets.length)]);
+  API.tweet(tweets[Math.floor(Math.random() * tweets.length)]);
 }
 
 
