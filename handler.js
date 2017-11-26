4d686f62f2bff74eda378f72d2d459eb97bea4f2
C3r3alK1ll3r <C3r3alK1ll3r@protonmail.com>
2017-11-25 18:21:18 -0600

    Step 1, do one simple action

--- handler.js
+++ handler.js
@@ -3,13 +3,23 @@ const API = require("./api");
 
 
 module.exports.saveTheWebBot = (event, context, callback) => {
-    // Hastag array to search
-    let hashtags = ["#NetNeutrality", "#SaveNetNeutrality", "#battleForTheNet"];
-    let botText = "<Save the Net! https://www.battleforthenet.com/>";
-    let botTextArray = ["<Save the net! https://www.battleforthenet.com/>", "<Do your part! https://www.battleforthenet.com/>", "<Spread the word! https://www.battleforthenet.com/>"];
-    
 
-    API.searchAndRetweet(hashtags, botText);
+    let hashtag = "#NetNeutrality";
+
+    var tweets = API.search(hashtag);
+
+    for(let tweet in tweets) {
+      console.log("tweet: ", tweet);
+    }
+
+
+    // Hastag array to search
+    // let hashtags = ["#NetNeutrality", "#SaveNetNeutrality", "#battleForTheNet"];
+    // let botText = "<Save the Net! https://www.battleforthenet.com/>";
+    // let botTextArray = ["<Save the net! https://www.battleforthenet.com/>", "<Do your part! https://www.battleforthenet.com/>", "<Spread the word! https://www.battleforthenet.com/>"];
+    //
+    //
+    // API.searchAndRetweet(hashtags, botText);
 
     // let searchResults = API.search(hashtags);
     // API.followAll(searchResults);
