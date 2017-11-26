80686bc158f240bedf584c4bef7d4d01e4b16d24
C3r3alK1ll3r <C3r3alK1ll3r@protonmail.com>
2017-11-25 20:45:36 -0600

    cleaned up retweet and set it wake up and retweet 3 trending hashtags

--- handler.js
+++ handler.js
@@ -1,32 +1,30 @@
 'use strict';
 const API = require("./api");
 
-
-module.exports.saveTheWebBot = (event, context, callback) => {
-
-    let hashtag = "#NetNeutrality";
-
-    API.search(hashtag, 2).then((tweets) => {
-      //console.log(tweets);
-
-      console.log("tweets[0].retweeted_status: ", tweets[0].retweeted_status);
-
+function retweetHashTags() {
+  const hashtags = ["#NetNeutrality", "#SaveNetNeutrality", "#BattleForTheNet", "#AjitPaiSucks", "#DefendNetNeutrality", "#SaveTheInternet"];
+  for(let i = 0; i < 3; i++) {
+    const random = (i === 0) ? 0 : Math.floor(Math.random() * hashtags.length);
+    console.log("random: ", random);
+    API.search(hashtags[random], 1).then((tweets) => {
       //I found I have to us id_str instead of just id. For some reseaon the two
       //  vars can very. id_str seems to the the original
       if(tweets[0].retweeted_status) {
-        console.log("tweets[0].retweeted_status.id_str: ", tweets[0].retweeted_status.id_str);
         API.retweet(tweets[0].retweeted_status.id_str);
       } else {
-        console.log("tweets[0].id_str: ", tweets[0].id_str);
         API.retweet(tweets[0].id_str);
       }
-
-
-
-
     }).catch((err) => {
       console.log("ERROR: ", err);
     });
+  }
+}
+
+
+module.exports.saveTheWebBot = (event, context, callback) => {
+
+
+  retweetHashTags();
 
     //console.log("tweets: ", tweets);
 
