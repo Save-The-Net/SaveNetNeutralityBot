3b56efcce7a56ccb3206df18f30b5cd5291ec071
C3r3alK1ll3r <C3r3alK1ll3r@protonmail.com>
2017-11-26 12:30:44 -0600

    did some refactoring

--- handler.js
+++ handler.js
@@ -1,32 +1,13 @@
 'use strict';
 const API = require("./api");
-
-function retweetHashTags() {
-  const hashtags = ["#NetNeutrality", "#SaveNetNeutrality", "#BattleForTheNet", "#AjitPaiSucks", "#DefendNetNeutrality", "#SaveTheInternet"];
-  for(let i = 0; i < 1; i++) {
-    //const random = (i === 0) ? 0 : Math.floor(Math.random() * hashtags.length);
-    //console.log("random: ", random);
-    const random = Math.floor(Math.random() * hashtags.length);
-    API.search(hashtags[random], 1).then((tweets) => {
-      //I found I have to us id_str instead of just id. For some reseaon the two
-      //  vars can very. id_str seems to the the original
-      if(tweets[0].retweeted_status) {
-        API.retweet(tweets[0].retweeted_status.id_str);
-      } else {
-        API.retweet(tweets[0].id_str);
-      }
-    }).catch((err) => {
-      console.log("ERROR: ", err);
-    });
-  }
-}
+const tweets = require("./tweets");
 
 function tweetAboutHowToFight() {
 
   const isps = ["Comcast", "AT&T", "Verzion", "Charter Sprectrum", "Cricket", "CenturyLink", "Sprint", "T-Mobile"];
 
   const tweets = [
-    "Do you really want to start paying " + isps[Math.floor(Math.random() * isp.length)] + " an ADDITION fee just to check your twitter feed? No? Then write and call your representatives now https://www.battleforthenet.com/ and them want to keep #NetNeutrality Then retweet to all your followers!",
+    "Do you really want to start paying " + isps[Math.floor(Math.random() * isps.length)] + " an ADDITION fee just to check your twitter feed? No? Then write and call your representatives now https://www.battleforthenet.com/ and them want to keep #NetNeutrality Then retweet to all your followers!",
     "Tell the White House That You Want To Keep #NetNeutrality! Sign this White House petition and retweet to your followers! https://tinyurl.com/yc5vp5m3 #SaveNetNeutrality",
     "Get Ready, December 7th is the Don't Kill the Net Protest at Verzion stores. You can find one in your area here: http://verizonprotests.com/. Get everyone you know, this isn't a partisan issue. This is keeping the Net an open space for innovation! #NetNeutrality",
     "Only five people at the FCC get to vote on Net Neutrality Repeal. Two plan to vote keep net neutrality. To defeat the net neutrality repeal one of these men MUST change their vote: Ajit Pai: 202-518-7399 Michael O'Rielly: 301-657-9092 Brendan Carr: 202-719-7305 #NetNeutrality",
@@ -34,13 +15,15 @@ function tweetAboutHowToFight() {
     "Have you called Congress today? https://www.battleforthenet.com/ Take Action Now Before It's To Late! Call and Retweet to #SaveTheNet #SaveNetNeutrality #NetNeutrality #SaveTheNetBot",
     "Add your own #NetNeutrality Campaign Bot to Your Twitter Account. https://github.com/Save-Net-Neutrality/SaveNetNeutralityBot #SaveTheNet by keeping the word moving while you're on the move, or taking a break. #SaveTheNetBot will help everyone spread info about how to #SaveNetNeutrality and call on your followers to #DefendTheNet"
   ];
-  API.tweet(tweets[Math.floor(Math.random() * tweets.length]);
+  API.tweet(tweets[Math.floor(Math.random() * tweets.length)]);
 }
 
 
 module.exports.saveTheWebBot = (event, context, callback) => {
 
-  retweetHashTags();
+  const hashtags = ["#NetNeutrality", "#SaveNetNeutrality", "#BattleForTheNet", "#AjitPaiSucks", "#DefendNetNeutrality", "#SaveTheInternet"];
+  tweets.retweetHashTags(hashtags, 1);
+
   tweetAboutHowToFight();
 
     //console.log("tweets: ", tweets);
