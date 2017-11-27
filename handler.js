239d798909c5abbdf5af2f442ef64fe232f6cf7c
C3r3alK1ll3r <C3r3alK1ll3r@protonmail.com>
2017-11-27 11:39:39 -0600

    updated

--- handler.js
+++ handler.js
@@ -3,9 +3,9 @@ const tAction = require("./tweet.actions");
 
 module.exports.saveTheWebBot = (event, context, callback) => {
   //pass in how many retweets you'd like your acccount to do each cycle
-  tAction.retweetHashTags(1);
+  tAction.retweetHashTags(3);
   //pass in how many informational tweets with links you'd like to post each cycle
-  tAction.howToFight(1);
+  tAction.howToFight(2);
   //This function tweets portions of "Do no go gentle into that good night"
   //the first parameter indicates how many tweets you'd like each cycle
   //the second paramter set to true will post the whole poerm
